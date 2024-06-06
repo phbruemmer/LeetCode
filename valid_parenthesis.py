b646a03e1f675fb10f2d7194c5b1da0f1d833723
phbruemmer <phbruemmer@gmail.com>
2024-06-06 19:36:40 +0200

    - Finished valid_parenthesis.py     - Memory and Runtime is a coinflip

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -4,11 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        opposite = {')': '(', '}': '{', ']': '['}
+        valid = True
+        last_opening_brackets = []
+
         for i in range(0, len(s)):
-            print(s[i])
+            current_bracket = s[i]
+            if current_bracket == '(' or current_bracket == '{' or current_bracket == '[':
+                last_opening_brackets.append(current_bracket)
+            elif current_bracket in opposite:
+                if last_opening_brackets and opposite[current_bracket] == last_opening_brackets[-1]:
+                    last_opening_brackets.pop(-1)
+                else:
+                    valid = False
+        if not len(last_opening_brackets) == 0:
+            valid = False
+        return valid
 
 
 solution_instance = Solution()
-parenthesisStr = "()[]{}"
-sol = solution_instance.isValid(parenthesisStr)
+parenthesis = "(("
+sol = solution_instance.isValid(parenthesis)
 print(sol)
