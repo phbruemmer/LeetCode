0fbd5dd91f3ee3652d4ae0e0ecd6c8b6b83be231
phbruemmer <phbruemmer@gmail.com>
2024-06-10 16:52:01 +0200

    - Finished PlusOne.py

--- PlusOne.py
+++ PlusOne.py
@@ -6,26 +6,25 @@ class Solution(object):
         """
         counter = 0
         digits.reverse()
-        last_num = 0
+        carry = 0
 
-        while counter < len(digits) -1:
-            new_digit = digits[counter] + 1
-            if new_digit >= 10 and digits[counter + 1] < 9:
-                last_num = new_digit
-                digits[counter] = new_digit % 10
+        digits[counter] += 1
+
+        while counter < len(digits):
+            digits[counter] += carry
+            carry = 0
+            current_digit = digits[counter]
+            if current_digit >= 10:
+                digits[counter] = current_digit % 10
+                carry += 1
                 if len(digits) <= (counter + 1):
                     digits.append(0)
-            else:
-                if counter == 0:
-                    digits[0] += 1
-                elif last_num >= 10:
-                    digits[counter] = new_digit
             counter += 1
+
         digits.reverse()
         return digits
 
 
-
 solution_instance = Solution()
-sol = solution_instance.plusOne([8,9,9,9])
+sol = solution_instance.plusOne([9])
 print(sol)
